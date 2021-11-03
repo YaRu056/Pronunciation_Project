bacd9d1118617e65d6e54b92a7b243b55463c075
YaRu056 <yaru1010514@gmail.com>
2021-11-03 17:51:06 +0800

    'Init'

---
+++ CreateInitial.py
@@ -0,0 +1,24 @@
+import numpy as np
+import json
+
+
+In_dict={}
+Fin_dict={}
+f=open('InitialPro.csv',"r")
+list1=f.readlines()
+Initial=['b','p','m','f','d','t','n','l','g','k','h','j','q','x','zh','ch','sh','r','z','c','s','y','w']
+
+print(Initial[6],Initial[7])
+for i in  range (len(Initial)):
+  In_dict[Initial[i]]={}
+  list2=list1[i+1].split(",")
+  for j in range (len(Initial)):
+    list3=list2[j].split(",")
+    list3=list(np.float_(list3))
+    In_dict[Initial[i]][Initial[j]]=list3[0]
+First="b"
+Second="w"
+
+output = json.dumps(In_dict)
+
+print(output)
