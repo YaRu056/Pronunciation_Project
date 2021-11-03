a0d01682fdb51b05c3f909d3bb004203fb810efc
YaRu056 <yaru1010514@gmail.com>
2021-11-03 18:43:05 +0800

    Update CreateTable

--- CreateInitial.py
+++ CreateInitial.py
@@ -1,24 +1,21 @@
-import numpy as np
 import json
 
-
-In_dict={}
-Fin_dict={}
 f=open('InitialPro.csv',"r")
-list1=f.readlines()
+line=f.readlines()
 Initial=['b','p','m','f','d','t','n','l','g','k','h','j','q','x','zh','ch','sh','r','z','c','s','y','w']
 
-print(Initial[6],Initial[7])
-for i in  range (len(Initial)):
-  In_dict[Initial[i]]={}
-  list2=list1[i+1].split(",")
-  for j in range (len(Initial)):
-    list3=list2[j].split(",")
-    list3=list(np.float_(list3))
-    In_dict[Initial[i]][Initial[j]]=list3[0]
-First="b"
-Second="w"
+index=line[0][1:-1].split(',')
+remain=line[1:]
+value=[]
 
-output = json.dumps(In_dict)
+for i in remain:
+  temp=i[:-1].split(',')
+  value.append(temp)
 
-print(output)
+_json = {
+  'index': index,
+  'values': value
+}
+#print(value)
+InitialTable = json.dumps(_json)
+print(InitialTable)
